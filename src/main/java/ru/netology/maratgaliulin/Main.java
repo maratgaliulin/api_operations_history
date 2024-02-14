92c58eefbfbaeac0cf1f6f0e7cb861d05d5fa42a
Marat Galiulin <egomarat@gmail.com>
2024-02-14 18:32:11 +0600

    Initial Commit

--- src/main/java/ru/netology/maratgaliulin/Main.java
+++ src/main/java/ru/netology/maratgaliulin/Main.java
@@ -2,7 +2,6 @@ package ru.netology.maratgaliulin;
 
 public class Main {
     public static void main(String[] args) {
-
         String name = "Marat Galiulin";
         System.out.println("Hello, " + name + "!");
     }
