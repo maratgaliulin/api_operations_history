e60694fcba810d55d9718abcb864588c709200ef
Marat Galiulin <egomarat@gmail.com>
2024-02-20 22:44:38 +0600

    Добавил Scanner

--- src/main/java/ru/netology/maratgaliulin/Main.java
+++ src/main/java/ru/netology/maratgaliulin/Main.java
@@ -1,8 +1,23 @@
 package ru.netology.maratgaliulin;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         String name = "Marat Galiulin";
         System.out.println("Hello, " + name + "!");
+
+        System.out.println("Введите строку: ");
+        String s = scan.nextLine();
+
+        System.out.println("Введите целое число: ");
+        int i = scan.nextInt();
+
+        System.out.println("Введите десятичное дробное число: ");
+        float f = scan.nextFloat();
+
+        System.out.println("Вы ввели: " + i + ", " + f + ", " + s);
+
     }
 }
